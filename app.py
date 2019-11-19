90912f35f9aa69e3833f652cb76a6dc20090eb6c
kuertzva <kuertzva@miamioh.edu>
2019-11-19 13:33:43 -0500

    fixed the import

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ creates the views for homepage and episode picker combined.
 
 from flask import Flask, request, render_template, json, session
 import requests
-from func_models import *
+from models import *
 import webbrowser
 from random import randint
 import os
